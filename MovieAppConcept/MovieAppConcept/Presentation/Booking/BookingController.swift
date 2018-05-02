425d61e38c8dab2b7734269630543f2e4e6c7019
own2pwn <evg2own@gmail.com>
2018-05-02 12:40:18 +0300

    heading

---
+++ MovieAppConcept/MovieAppConcept/Presentation/Booking/BookingController.swift
@@ -0,0 +1,38 @@
+//
+//  BookingController.swift
+//  MovieAppConcept
+//
+//  Created by Evgeniy on 02.05.18.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+final class BookingController: UIViewController {
+
+    // MARK: - Outlets
+    
+    // MARK: - Overrides
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupScreen()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+    
+    // MARK: - Methods
+    
+    private func setupScreen() {
+        setup()
+    }
+    
+    private func setup() {
+        let setup = [setColors]
+        setup.forEach { $0() }
+    }
+    
+    private func setColors() {
+    }
+}
