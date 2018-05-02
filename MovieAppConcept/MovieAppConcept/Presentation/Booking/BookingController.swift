12206910319ece85e7ab00f21768e471e0f5c792
own2pwn <evg2own@gmail.com>
2018-05-02 12:53:08 +0300

    addHorizontalSeparator

--- MovieAppConcept/MovieAppConcept/Presentation/Booking/BookingController.swift
+++ MovieAppConcept/MovieAppConcept/Presentation/Booking/BookingController.swift
@@ -12,6 +12,8 @@ final class BookingController: UIViewController {
 
     // MARK: - Outlets
     
+    @IBOutlet var heading: UIView!
+    
     // MARK: - Overrides
     
     override func viewDidLoad() {
@@ -29,10 +31,39 @@ final class BookingController: UIViewController {
     }
     
     private func setup() {
-        let setup = [setColors]
+        let setup = [setColors, setupHeading]
         setup.forEach { $0() }
     }
     
     private func setColors() {
+    }
+    
+    private func setupHeading() {
+        let margin: CGFloat = 16
+        let containerSize = heading.frame.size
+        
+        let separatorLayer = CALayer()
+        separatorLayer.frame = CGRect(origin: CGPoint(x: margin, y: containerSize.height), size: CGSize(width: containerSize.width - 2 * margin, height: 1))
+        separatorLayer.backgroundColor = UIColor.red.cgColor
+        heading.layer.addSublayer(separatorLayer)
+    }
+}
+
+public extension UIView {
+    public enum SeparatorPosition {
+        case top, bot
+    }
+    
+    public func addHorizontalSeparator(at position: SeparatorPosition, margin: CGFloat) {
+        let separatorLayer = CALayer()
+        let mySize = bounds.size
+        
+        let yPosition: CGFloat = position == .top ? 0 : mySize.height
+        let origin = CGPoint(x: margin, y: yPosition)
+        
+        separatorLayer.frame = CGRect(origin: origin, size: CGSize(width: mySize.width - 2 * margin, height: 1))
+        separatorLayer.backgroundColor = UIColor.red.cgColor
+        
+        layer.addSublayer(separatorLayer)
     }
 }
