2b73367068f50346e278331d67c44869de25ccd6
own2pwn <evg2own@gmail.com>
2018-05-03 21:41:07 +0300

    v3

--- MovieAppConcept/MovieAppConcept/Presentation/Booking/Model/SeatType.swift
+++ MovieAppConcept/MovieAppConcept/Presentation/Booking/Model/SeatType.swift
@@ -11,17 +11,17 @@ import UIKit
 public typealias SeatArray = [[SeatType]]
 
 public enum LineType {
-    case normal([BlockType])
+    case normal // ([BlockType])
     case empty
 }
 
 public enum BlockType {
-    case normal([SeatType_])
+    case normal // ([SeatType_])
     case empty
 }
 
-public enum SeatType_ {
-    case normal, booked
+public enum SeatType {
+    case regular, booked
     case empty
 }
 
@@ -30,21 +30,31 @@ public struct Stage {
 }
 
 public struct Line {
-    let type: LineType
+    let blocks: [Block]
+    // let spacing: CGFloat
 }
 
 public struct Block {
-    let type: BlockType
-}
-
-public struct Seat {
-    let type: SeatType_
+    let seats: [SeatType_]
+    // let spacing: CGFloat
 }
 
 final class Test {
     func provider() {
-        let b1 = BlockType.normal([.empty, .empty, .normal])
-        let b2 = BlockType.empty
+        // let b1 = BlockType.normal([.empty, .empty, .normal])
+        // let b2 = BlockType.empty
+
+        // 1. get max count of seats
+        // 2. calculae seat size so all of the same size, here we accept seat spacing (itemSpacing)
+        // 2. take into account space between lines and blocks
+        let blockSpacing: CGFloat = 4
+
+        let b1 = Block(seats: [.empty, .empty, .regular])
+        let b2 = Block(seats: [.empty, .regular, .regular, .regular, .regular, .empty])
+        let b3 = Block(seats: [.regular])
+        
+        let l1 = Line(blocks: [b1, b2, b3])
+        let
     }
 }
 
@@ -63,12 +73,12 @@ public struct SeatModel {
     let spacing: CGFloat
 }
 
-public enum SeatType {
+public enum _SeatType {
     case none, emptyRow, available, booked
     case setSpace(CGFloat), rowSpace(CGFloat)
 }
 
-extension SeatType {
+extension _SeatType {
     var shouldRender: Bool {
         switch self {
         case .available, .booked:
